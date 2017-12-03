eee8952135046ecc9b00379786c43f21979d0d55
lenovo <532973536@qq.com>
2017-12-03 16:31:15 +0800

    遮罩层

---
+++ requirejs/弹出层/js/index.js
@@ -0,0 +1,21 @@
+/**
+ * Created by lenovo on 2017/12/3.
+ */
+requirejs.config({
+    paths: {
+        jquery: 'jquery-3.2.1'
+    }
+});
+require(['jquery','dialog'],function ($,Dialog) {
+    $('#open').on('click',function () {
+       var setting = {
+           width:300,
+           height:200,
+           title:'注册',
+           content:'reigist.html'
+       };
+
+       var dl = new Dialog()
+        dl.open(setting);
+    });
+});
