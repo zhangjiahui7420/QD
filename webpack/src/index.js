aaa1f7885b275fa715a0711808b889a5c4cf2038
lenovo <532973536@qq.com>
2018-01-25 15:39:14 +0800

    webpack

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -1,2 +1,3 @@
 import	css from './css/index.css';
+import sass from './css/commom.scss';
 var a = 5;
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,17 +1,20 @@
 const path = require('path');
 const HtmlPlugin = require('html-webpack-plugin');
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const entry = require("./webpack_config/entry_webpack.js");
 module.exports = {
     //入口文件
-    entry:{
-        index:'./src/index.js',  //对应出口里面的[name]
+    entry:entry,
+        
+        // index:'./src/index.js',  //对应出口里面的[name]
         // entry2:'./src/entry2.js'
-    },
+    
     //出口文件
     output:{
         path:path.resolve(__dirname,'dist'),
-        filename:'[name].js'
+        filename:'[name].js',
+        publicPath:'http://127.0.0.1:8081/'
     },
     //模块
     module:{
@@ -32,8 +35,33 @@ module.exports = {
                         outputPath:'images/'
                     }
                 }]
-            }
-        ]
+            },{
+                test:/\.(htm|html)$/i,
+                loader:'html-withimg-loader'
+            },{
+                test:/\.scss/,
+                use:[{
+                    loader:'style-loader'
+                },{
+                    loader:'css-loader'
+                },{
+                    loader:'sass-loader'
+                }
+                ]
+                },{
+                   test:/\.(jsx|js)$/,
+                   use:{
+                       loader:'babel-loader',
+                       options:{
+                           presets:[
+                               "es2015",
+                               "react"
+                           ]
+                       }
+                   },
+                   exclude:/node_modules/  //排除
+              }
+          ]
     },
     //插件
     plugins:[
@@ -45,14 +73,14 @@ module.exports = {
             template:'./src/index.html'
         }),
         new ExtractTextPlugin("css/index.css"),
-        new UglifyJsPlugin()
+        new UglifyJsPlugin(),
     ],
     //配置webpack开发服务功能
     devServer:{
         //设置基本目录结构
         contentBase: path.resolve(__dirname,'dist'),
         //服务器的IP地址，可以使用IP也可以使用localhost
-        host:'localhost',
+        host:'127.0.0.1',
         //服务端压缩是否开启
         compress:true,
         //配置服务端口号
