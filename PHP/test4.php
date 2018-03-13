24efba0a8268ebd8981661fdac5dd56c03ec85dc
lenovo <532973536@qq.com>
2018-03-13 21:32:59 +0800

    test

---
+++ PHP/test4.php
@@ -0,0 +1,11 @@
+<?php
+    $arr['02-11']='外地出差';
+    $arr['03-23']='看美剧';
+    $arr['04-21']='打游戏'；
+
+?>
+
+<meta charset="utf-8">
+<h3>单机查询当日日程</h3>
+<form action="test4.php" method="post">
+    
